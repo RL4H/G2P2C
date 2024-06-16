fix: modification to DDPG to remove layer normalisation

agents/ddpg/ddpg.py
@@ -22,6 +22,9 @@ from collections import namedtuple, deque
 
 # python run_RL_agent.py --agent ddpg --folder_id Experiment2_NoiseModel/Model_8/Limit_1/DecayingSigma_5e-1_rev1/DDPG2_3 --patient_id 2 --return_type average --action_type exponential --device cuda --pi_lr 1e-4 --vf_lr 1e-3 --soft_tau 0.001 --noise_model ou_noise --noise_std 5e-1  --seed 3 --debug 0
 
+# python run_RL_agent.py --agent ddpg --folder_id DDPG_LayerNorm_PostActivation_WithPenalty/OUNoise_Sigma_5e-1/DDPG2_3 --patient_id 2 --return_type average --action_type exponential --device cuda --pi_lr 1e-4 --vf_lr 1e-3 --soft_tau 0.001 --noise_model ou_noise --noise_std 5e-1  --mu_penalty 1 --seed 3 --debug 0
+
+
 Transition = namedtuple('Transition',
                         ('state', 'feat', 'action', 'reward', 'next_state', 'next_feat', 'done'))
 
@@ -198,9 +201,7 @@ class DDPG:
             done_batch = torch.cat(batch.done).unsqueeze(1)
 
             # value network (critic) update
-            self.ddpg.value_net.train()
             new_action, next_log_prob = self.ddpg.evaluate_target_policy_no_noise(next_state_batch, next_feat_batch)
-            self.ddpg.value_net_target.eval()
 
             next_values = self.ddpg.value_net_target(next_state_batch, next_feat_batch, new_action)
             target_value = (reward_batch + (self.gamma * (1 - done_batch) * next_values))

agents/ddpg/models.py
@@ -64,9 +64,9 @@ class QvalModel(nn.Module):
         self.fc_layer2 = nn.Linear(self.last_hidden, self.last_hidden)
         self.fc_layer3 = nn.Linear(self.last_hidden, self.last_hidden)
 
-        self.bn_layer1 = nn.LayerNorm(self.last_hidden)
-        self.bn_layer2 = nn.LayerNorm(self.last_hidden)
-        self.bn_layer3 = nn.LayerNorm(self.last_hidden)
+        # self.bn_layer1 = nn.LayerNorm(self.last_hidden)
+        # self.bn_layer2 = nn.LayerNorm(self.last_hidden)
+        # self.bn_layer3 = nn.LayerNorm(self.last_hidden)
 
         self.q = nn.Linear(self.last_hidden, self.output)
 
@@ -74,13 +74,13 @@ class QvalModel(nn.Module):
         concat_dim = 1 if (mode == 'batch') else 0
         concat_state_action = torch.cat((extract_state, action), dim=concat_dim)
         fc_output1 = F.relu(self.fc_layer1(concat_state_action))
-        fc_output1 = self.bn_layer1(fc_output1)
+        # fc_output1 = self.bn_layer1(fc_output1)
 
         fc_output2 = F.relu(self.fc_layer2(fc_output1))
-        fc_output2 = self.bn_layer1(fc_output2)
+        # fc_output2 = self.bn_layer2(fc_output2)
 
         fc_output = F.relu(self.fc_layer3(fc_output2))
-        fc_output = self.bn_layer1(fc_output)
+        # fc_output = self.bn_layer3(fc_output)
 
         qval = self.q(fc_output)
         return qval
@@ -104,9 +104,9 @@ class ActionModule(nn.Module):
         self.fc_layer2 = nn.Linear(self.last_hidden, self.last_hidden)
         self.fc_layer3 = nn.Linear(self.last_hidden, self.last_hidden)
 
-        self.bn_layer1 = nn.LayerNorm(self.last_hidden)
-        self.bn_layer2 = nn.LayerNorm(self.last_hidden)
-        self.bn_layer3 = nn.LayerNorm(self.last_hidden)
+        # self.bn_layer1 = nn.LayerNorm(self.last_hidden)
+        # self.bn_layer2 = nn.LayerNorm(self.last_hidden)
+        # self.bn_layer3 = nn.LayerNorm(self.last_hidden)
 
         self.mu = nn.Linear(self.last_hidden, self.output)
         # self.mu = NormedLinear(self.last_hidden, self.output, scale=0.1)
@@ -119,22 +119,22 @@ class ActionModule(nn.Module):
 
     def forward(self, extract_states, worker_mode='training'):
         fc_output1 = F.relu(self.fc_layer1(extract_states))
-        if worker_mode == 'training':
-            fc_output1 = self.bn_layer1(fc_output1)
+        # if worker_mode == 'training':
+        #     fc_output1 = self.bn_layer1(fc_output1)
 
         fc_output2 = F.relu(self.fc_layer2(fc_output1))
-        if worker_mode == 'training':
-            fc_output2 = self.bn_layer2(fc_output2)
+        # if worker_mode == 'training':
+        #     fc_output2 = self.bn_layer2(fc_output2)
 
         fc_output = F.relu(self.fc_layer3(fc_output2))
-        if worker_mode == 'training':
-            fc_output = self.bn_layer3(fc_output)
+        # if worker_mode == 'training':
+        #     fc_output = self.bn_layer3(fc_output)
 
         mu = self.mu(fc_output)
         sigma = self.sigma(fc_output)  # * 0.66, + 1e-5
         log_std = torch.clamp(sigma, LOG_STD_MIN, LOG_STD_MAX)
         action_std = torch.exp(log_std)
-        dst = self.normalDistribution(mu, action_std)
+        # dst = self.normalDistribution(mu, action_std)
 
         if worker_mode == 'training':
             noise_value = self.policy_noise.get_noise()
@@ -349,7 +349,7 @@ class ExploratoryNoise:
 
     def get_noise(self):
         if self.noise_model == 'normal_dist':
-            return self.noise
+            return torch.distributions.Normal(0, self.sigma).rsample()
 
         elif self.noise_model == 'ou_noise':
             x = self.x_prev + self.theta * (self.mu - self.x_prev) * self.dt \