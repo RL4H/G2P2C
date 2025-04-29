124020537516cf8ee503ac3c7dd9c8b3f4e993ff
eoeldroal <eoelroal@naver.com>
2025-04-29 18:17:31 +0900

    chore: add __init__.py to Sim_CLI to make it a package for imports

---
+++ Sim_CLI/__init__.py
@@ -0,0 +1,7 @@
+"""
+Package initializer for Sim_CLI package.
+"""
+# This file allows Sim_CLI to be recognized as a Python package.
+# It can be left empty, but is required for package imports.
+ 
+__all__ = []
